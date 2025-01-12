6ad2790a90f815b7696c7e76e7601ce01a227798
DinandCode <dinandaristo16@gmail.com>
2025-01-12 19:14:47 +0700

    update link whatsapp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,10 +34,12 @@
         <div class="row align-items-center mb-5">
             <div class="col-md-6">
                 <h2 class="text-danger fw-bold">Order lewat mana saja</h2>
-                <p class="fs-5">Anda dapat menghubungi kami melalui DM Instagram, WhatsApp, Email, atau langsung melalui website ini untuk memesan produk SITAARA.</p>
+                <p class="fs-5">Anda dapat menghubungi kami melalui DM Instagram, WhatsApp, Email, atau langsung melalui website ini untuk memesan produk ELYSEIA.</p>
             </div>
             <div class="col-md-6 text-center">
-                <img src="/img/home/order-options.png" alt="Cara Order" class="img-fluid rounded-circle shadow">
+                <a href="https://wa.me/+6287834440508">
+                    <img src="/img/home/icons8-whatsapp.svg" alt="Cara Order" class="img-fluid rounded-circle shadow" >
+                </a>
             </div>
         </div>
 
